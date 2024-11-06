1596ff912641237a1269d3704d4b62b9f835b0a0
Aiden <aiden.chandran@elev.ga.lbs.se>
2024-11-06 11:16:48 +0100

    Transition

---
+++ Assets/level_loader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class level_loader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(Loadlevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator Loadlevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+    
+
+    
+}
