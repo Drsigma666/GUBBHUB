03b23ffb66d538c3550d0f8d782073801b62a2cb
Aiden <aiden.chandran@elev.ga.lbs.se>
2024-11-07 14:09:57 +0100

    asfnafsaf

--- Assets/Main_menu.cs
+++ Assets/Main_menu.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
 
 public class Main_menu : MonoBehaviour
 {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     public void playGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    IEnumerator LoadScene(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
