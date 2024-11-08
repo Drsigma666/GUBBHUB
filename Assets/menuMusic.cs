9d818817666ccebcd135e40b88f5c90018b0c31f
Mimoen <mimo.lowenmark@gmail.com>
2024-11-08 12:18:43 +0100

    menu music

---
+++ Assets/menuMusic.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuMusic : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            Debug.LogError("No AudioSource found on the GameObject. Please add an AudioSource component.");
+            return;
+        }
+
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
